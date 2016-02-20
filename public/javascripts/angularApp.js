d5e828c6d5d9ec4db75b9711ea6ca8fb08d9ad9b
Paula Lehtola <pklehtol@cs.helsinki.fi>
2016-02-20 19:54:10 +0200

    secure commit, don't remember what it is

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,7 +1,22 @@
 var app = angular.module('journeyPlanner', []);
 
+
+app.factory('routes', [function(){
+  var o = {
+    routes: []
+  };
+  return o;
+}]);
+
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'routes',
+function($scope, routes){
   $scope.date = new Date();
+  $scope.routes = routes.routes;
+  $scope.addRoutes = function(){
+    $scope.routes.push({type: 1, stop1: 12345, stop2: 56789, startTime: "19:00:00", endTime: "20:00:00"});   
+  };
+    
 }]);
